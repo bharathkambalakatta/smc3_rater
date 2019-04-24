8e64ac8ad32ab29e047de3e35a4d62166627d552
Nutan Priyadarshini <nutan.p@dreamorbit.com>
2019-04-24 16:48:18 +0530

    Added a write file method to property file

--- src/main/java/com/dreamorbit/SMC3_Rater/testutils/PropertyFileUtility.java
+++ src/main/java/com/dreamorbit/SMC3_Rater/testutils/PropertyFileUtility.java
@@ -3,6 +3,7 @@ package com.dreamorbit.SMC3_Rater.testutils;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -29,6 +30,28 @@ public class PropertyFileUtility {
 		}
 
 	}
+
+	/**
+	 * Method to write data into a property file 
+	 * @author nutan.p
+	 *
+	 */
+	
+		public void addData(String key, String val) 
+		{
+			try {
+			 File file = new File("./Properties/"+"/BidNameFile.properties");  
+			 Properties props = new Properties();
+			 props.load(new FileInputStream(file));
+			FileOutputStream obj = new FileOutputStream(file);
+			 props.setProperty(key, val);
+			props.store(obj, "Update data into file ");
+		}
+			 catch (IOException ex) {
+		         ex.printStackTrace();
+		}
+		}
+
 
 	public String getValue(String value) {
 		return prop.getProperty(value);
